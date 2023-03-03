88e72fe27041526c3253d461bd3f55872058034f
William Gao <williamgao@Williams-MacBook-Pro.local>
2023-03-02 23:26:50 -0800

    a better solution beats 90%

--- lt283_move_zeros.py
+++ lt283_move_zeros.py
@@ -9,16 +9,19 @@
 # Input: nums = [0,1,0,3,12]
 # Output: [1,3,12,0,0]
 
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
+class Solution(object):
+    def moveZeroes(self, nums):
         """
-        Do not return anything, modify nums in-place instead.
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
         """
-        # do not go by 0, instead, when there is non zero, swap it with the first zero 
 
-        zero = 0; #default to first position, assume it is 0
-
+        # snowball
+        ball = 0
         for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[i], nums[zero] = nums[zero], nums[i]
-                zero += 1 # move to next until next 0 found 
+            if nums[i] == 0: 
+                ball += 1 
+            else:
+                nums[i-ball], nums[i] = nums[i], nums[i-ball]
+        
+        return nums 
